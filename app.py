b64fa7b5da7d0d18ed318f0d6a9082fdcc87f654
an4nd-sudo <anandps0809@gmail.com>
2024-09-25 21:13:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,11 @@ with st.expander('show data'):
 #with st.sidebar.selectbox:
    # op = st.multiselect('select y axis,'[1,2,3])
 
-op = st.sidebar.selectbox('select options',[1,2,3])
+#op = st.sidebar.selectbox('select options',[1,2,3])
+
+regions = ['Ahmedabad','Banglore','Chennai']
+
+op = st.sidebar.selectbox('select region',[1,2,3],format_func=lambda x: regions[x-1])
 
 df2 = df[df['Region']==op] 
 st.write(df2)
@@ -36,6 +40,4 @@ col3.metric('Total Delicassen',df3['Delicassen'].sum())
 fig = px.bar(x=['Fresh','Milk','Grocery','Frozen','Detergents_Paper','Delicassen'], y=[df3['Fresh'].sum(),df3['Milk'].sum(),df3['Grocery'].sum(),df3['Frozen'].sum(),df3['Detergents_Paper'].sum(),df3['Delicassen'].sum()])
 st.plotly_chart(fig)
 
-regions = ['Ahmedabad','Banglore','Chennai']
 
-op3 = st.sidebar.selectbox('select region',[1,2,3],format_func=lambda x: regions[x-1])
